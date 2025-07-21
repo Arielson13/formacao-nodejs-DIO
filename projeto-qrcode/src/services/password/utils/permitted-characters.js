d6d9c4ee8e2af07b5f4f60307f3fd1ac36b0c7dd
Arielson13 <arielsonsousa37@gmail.com>
2025-07-21 14:29:12 -0300

    Mais um projeto da formação de nodejs da DIO concluído.

---
+++ projeto-qrcode/src/services/password/utils/permitted-characters.js
@@ -0,0 +1,16 @@
+async function permittedCharacters(){
+    let permitted = [];
+
+    if(process.env.UPPERCASE_LATTERS === "true")
+        permitted.push(..."ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    if(process.env.LOWERCASE_LATTERS === "true")
+        permitted.push(..."abcdefghijklmnopqrstuvwxyz");
+    if(process.env.NUMBERS === "true") 
+        permitted.push(..."0123456789");
+    if(process.env.SPECIAL_CHARACTERS === "true")
+        permitted.push(..."@#$%^&*()-_");
+
+    return permitted;
+}
+
+export default permittedCharacters;
